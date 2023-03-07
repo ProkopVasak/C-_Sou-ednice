c4f19e00587f3d81f321663be3ac2ec57a8ddffd
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2023-03-07 18:20:11 +0100

    Add files via upload

---
+++ Souradnice/Bod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souradnice
+{
+    internal class Bod 
+    {
+        public Bod(int x, int y )
+        {
+            X = x;
+            Y = y;
+
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public double Size { get { return Math.Sqrt(X * X + Y * Y); } }
+        /*public Bod Násobení(int z)
+        {
+            Bod c = new Bod(X * z, Y * z);
+            return c;
+        }
+        public Bod Sčítání(Bod x,Bod y)
+        {
+            Bod d = new Bod(x.X+y.X,x.Y+y.Y);
+            return d;
+        }
+        public static bool Equals(Bod x , Bod y)
+        {
+            if (x.X.Equals(y.X) && x.Y.Equals(y.Y)) return true;
+            else return false;
+        }
+        public static string KdoJeDál(Bod x,Bod y)
+        {
+            if (x.Size > y.Size) return "1. bod je dále od počátku.";
+            else if (Equals(x, y)) return "Body jsou stejně daleko od počátku.";
+            else return "2. bod je dále od počátku.";
+        }*/
+    } 
+}
---
+++ Souradnice/Program.cs
@@ -0,0 +1,10 @@
+﻿using Souradnice;
+Bod c1 = new Bod(10, 10);
+Bod c2 = new Bod(10, 10);
+  
+//if (c1.X.Equals(c2.X)) Console.WriteLine("negr");
+//else if (!c1.X.Equals(c2.X)) Console.WriteLine("nenegr");
+Console.WriteLine( Bod.Equals(c1, c2)); // Equals
+Console.WriteLine(Bod.KdoJeDál(c1,c2)); // porovná kdo je dál
+Console.WriteLine(Bod.Equals(c1.Násobení(4), c2.Násobení(4))); 
+
