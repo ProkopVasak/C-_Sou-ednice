01637f5393306dfdd656aa044b3dac7447fa7e88
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2023-03-07 18:21:07 +0100

    Update Program.cs

--- Souradnice/Program.cs
+++ Souradnice/Program.cs
@@ -2,8 +2,8 @@
 Bod c1 = new Bod(10, 10);
 Bod c2 = new Bod(10, 10);
   
-//if (c1.X.Equals(c2.X)) Console.WriteLine("negr");
-//else if (!c1.X.Equals(c2.X)) Console.WriteLine("nenegr");
+//if (c1.X.Equals(c2.X)) Console.WriteLine("ne");
+//else if (!c1.X.Equals(c2.X)) Console.WriteLine("ne");
 Console.WriteLine( Bod.Equals(c1, c2)); // Equals
 Console.WriteLine(Bod.KdoJeDál(c1,c2)); // porovná kdo je dál
 Console.WriteLine(Bod.Equals(c1.Násobení(4), c2.Násobení(4))); 
